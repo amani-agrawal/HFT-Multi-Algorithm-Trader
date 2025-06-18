7dcb3847057f5c67e95adf3397c17de801fcdb12
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2025-06-17 22:47:39 -0400

    Update market_maker.py

--- strategies/market_maker.py
+++ strategies/market_maker.py
@@ -22,7 +22,7 @@ def compute_spread(best_ask, best_bid, alpha=0.9):
 def round_to_tick(price):
     return round(price / TICK_SIZE) * TICK_SIZE
 
-def quote_spread(orderbook, inventory, spread, qty=0.0001, inventory_sensitivity=0.1):
+def quote_spread(orderbook, inventory, spread, qty=1, inventory_sensitivity=0.1):
     best_bid, best_ask = get_top_of_book(orderbook)
     mid_price = (best_bid[0] + best_ask[0]) / 2
     inv_skew = inventory_sensitivity * inventory
@@ -44,7 +44,7 @@ def run_market_maker(limit):
     realized_pnl_log = []
     unrealized_pnl_log = []
     total_pnl_log = []
-    mid_history = deque(maxlen=10)
+    mid_history = deque(maxlen=5)
     volatility = 0
 
     for _ in range(limit):
@@ -53,22 +53,10 @@ def run_market_maker(limit):
         mid_price_now = (best_bid[0] + best_ask[0]) / 2
         mid_history.append(mid_price_now)
 
-        #Logging
-        unrealized = inventory*mid_price_now
-        realized = cash
-
-        realized_pnl_log.append(realized)
-        unrealized_pnl_log.append(unrealized)
-        total_pnl_log.append(realized + unrealized)
-
-        if realized + unrealized < MAX_DRAWDOWN:
-            stop_trading(f"Total realized + unrealized: ${realized + unrealized}")
-
-
         #Calculating Spread
         spread = compute_spread(best_ask, best_bid)
         spread_pct = (best_ask[0] - best_bid[0]) / best_bid[0]
-        if len(mid_history) == 10:
+        if len(mid_history) == 5:
             volatility = np.std(mid_history)
             spread += 2 * volatility
         if volatility > MAX_VOLATILITY or spread_pct > MAX_SPREAD_PCT:
@@ -90,13 +78,25 @@ def run_market_maker(limit):
 
         if inventory > -MAX_INVENTORY and quote["sell_order"]["price"] * quote["sell_order"]["qty"] >= MIN_NOTIONAL:
             filled_sell, trades_sell = simulate_limit_order("sell", quote["sell_order"]["price"], quote["sell_order"]["qty"])
-
+        
         #Updating according to trades made
         cash -= sum(price * qty for price, qty in trades_buy)
         inventory += filled_buy
         cash += sum(price * qty for price, qty in trades_sell)
         inventory -= filled_sell
         time.sleep(1)
+
+        #Logging
+        unrealized = inventory*mid_price_now
+        realized = cash
+        print(unrealized, ",", realized, "-", mid_price_now)
+        realized_pnl_log.append(realized)
+        unrealized_pnl_log.append(unrealized)
+        total_pnl_log.append(realized + unrealized)
+
+        if realized + unrealized < MAX_DRAWDOWN:
+            stop_trading(f"Total realized + unrealized: ${realized + unrealized}")
+
 
     #Getting the final amount
     final_ob = get_live_orderbook()
@@ -110,13 +110,11 @@ def run_market_maker(limit):
     final_realized_pnl = cash
 
     realized_pnl_log.append(final_realized_pnl)
-    unrealized_pnl_log.append(final_realized_pnl)
+    unrealized_pnl_log.append(0.0)
     total_pnl_log.append(final_realized_pnl)
 
     return {
         "realized_pnl_log": realized_pnl_log,
         "unrealized_pnl_log": unrealized_pnl_log,
-        "total_pnl_log": total_pnl_log,
-        "final_realized_pnl": final_realized_pnl,
-        "final_inventory": inventory
+        "total_pnl_log": total_pnl_log
     }
