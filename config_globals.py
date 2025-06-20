3113deb094ece94b3ce8dadcdc032c660dfb9234
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2025-06-20 18:14:27 -0400

    Update config_globals.py

--- config_globals.py
+++ config_globals.py
@@ -1,8 +1,11 @@
 MAX_INVENTORY = 0.05         # Maximum long or short inventory
-MAX_DRAWDOWN = -10.0        # Max allowed PnL loss before halting
+MAX_DRAWDOWN = -100.0        # Max allowed PnL loss before halting
 MAX_SPREAD_PCT = 0.015      # Max spread (% of best bid) to quote into
 MAX_VOLATILITY = 20
 TICK_SIZE = 0.01            # smallest unit to trade
 COOLDOWN_PERIOD = 2         # seconds
 MIN_NOTIONAL = 10           # minimum trade amount (in $)
 PRICE_TOLERANCE = 0.05      # For real world simulation for accepted market orders
+WINDOW = 10
+THRESHOLD = 0.1
+INVENTORY_SENSITIVITY = 0.1
