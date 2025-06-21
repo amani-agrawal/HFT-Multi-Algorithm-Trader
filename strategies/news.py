3133e6dfda436d2a2a97c6dbcfeb6efaa080d4e9
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2025-06-20 21:36:26 -0400

    Create news.py

---
+++ strategies/news.py
@@ -0,0 +1,58 @@
+import finnhub
+from datetime import datetime, timedelta
+from config_globals import FINNUB_API_KEY, NEWS_THRESHOLD
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import torch
+from torch.nn.functional import softmax
+
+#to get news
+finnhub_client = finnhub.Client(api_key=FINNUB_API_KEY) 
+
+#to extract sentiment from news
+tokenizer = AutoTokenizer.from_pretrained("yiyanghkust/finbert-tone")   
+model = AutoModelForSequenceClassification.from_pretrained("yiyanghkust/finbert-tone") 
+
+#to extract company name from news header
+ner = pipeline("ner", model="dslim/bert-base-NER", grouped_entities=True)   
+
+today = datetime.utcnow().date()
+yesterday = today - timedelta(days=1)
+
+#The companyName should be its stock name like AAPL for APPLE
+def get_specific_news(companyName):
+    return finnhub_client.company_news(companyName, _from=yesterday.isoformat(), to=today.isoformat())
+
+# Category can be: 'general', 'forex', 'crypto', or 'merger'
+def get_general_news(category="general"):
+    yesterday_start = datetime.combine(yesterday, datetime.min.time())
+    yesterday_unix = int(yesterday_start.timestamp())
+    news = finnhub_client.general_news(category)
+    recent_news = [item for item in news if item['datetime'] >= yesterday_unix]
+    for idx, i in enumerate(recent_news):
+        if not i['related']:
+            companies = [e['word'] for e in ner(i['headline']) if e['entity_group'] == 'ORG']
+            for j in companies:
+                if len(j)>=20:
+                    continue
+                results = finnhub_client.symbol_lookup(j)['result']
+                if results and results[0]["symbol"]:
+                    recent_news[idx]["related"] = results[0]["symbol"]
+                    break
+    return recent_news
+
+def get_sentiment(news):
+    if not news['related']:
+        print("No company name specified.")
+        return ('HOLD', None)
+    text = news['headline'] + " " + news['summary']
+    inputs = tokenizer(text, return_tensors="pt", truncation=True)
+    outputs = model(**inputs)
+    probs = softmax(outputs.logits, dim=1)[0]
+    labels = ['positive', 'neutral', 'negative']
+    sentiment = {label: float(prob) for label, prob in zip(labels, probs)}
+    if sentiment['positive'] > NEWS_THRESHOLD:
+        return ('BUY', news['related'])
+    elif sentiment['negative'] > NEWS_THRESHOLD:
+        return ('SELL', news['related'])
+    else:
+        return ('HOLD', news['related'])
