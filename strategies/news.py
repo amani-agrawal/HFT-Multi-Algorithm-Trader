b6fbbd8157bd78ec42eaf28e97a9154b779964de
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2025-08-29 16:24:06 +0530

    Update news.py

--- strategies/news.py
+++ strategies/news.py
@@ -3,10 +3,12 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from torch.nn.functional import softmax
 from stream.finnub_news_streamer import get_general_news, get_specific_news
 from engine.matcher import simulate_order
+from collections import defaultdict
 
 #to extract sentiment from news
 tokenizer = AutoTokenizer.from_pretrained("yiyanghkust/finbert-tone")   
 model = AutoModelForSequenceClassification.from_pretrained("yiyanghkust/finbert-tone") 
+news_circulated = defaultdict(bool)
 
 def run_news_trend():
     news= get_general_news("general")
@@ -19,6 +21,10 @@ def get_sentiment(news):
     if not news['related']:
         print("No company name specified.")
         return ('HOLD', None)
+    if news_circulated[news['headline']]:
+        print("News already processed.")
+        return ('HOLD', None)
+    news_circulated[news['headline']]= True
     text = news['headline'] + " " + news['summary']
     inputs = tokenizer(text, return_tensors="pt", truncation=True)
     outputs = model(**inputs)
